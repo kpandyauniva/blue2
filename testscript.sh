c5414bbe0376fe497176a640f41048bce1b42f38
kpandyauniva <kpandya@univa.com>
2016-08-15 22:07:31 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,7 +2,7 @@
 while [ ! -f ServiceAccount.json ]
 do
         echo 'waiting...' 
-        sleep 30
+        sleep 60
 done
 cp ServiceAccount.json /opt/unicloud/config
 echo "done.." > /root/message.txt
