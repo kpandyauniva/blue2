c9d6b481d50cf3eb88c1abe8a36382effb3694d8
kpandyauniva <kpandya@univa.com>
2016-08-16 09:18:00 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-while [ ! -f /tmp/ServerAccount.json ]
+while [ ! -f /tmp/ServiceAccount.json ]
 do
         echo "waiting.." >> /tmp/update.txt
         sleep 60
 done
 
-cp /tmp/ServerAccount.json /opt/unicloud/config
+cp /tmp/ServiceAccount.json /opt/unicloud/config
+echo "ServiceAccount file added to Unicloud"
